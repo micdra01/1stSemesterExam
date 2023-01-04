b4fa44ed976d84ed9a0c8cc311bc96c7a0292b1f
micdra01 <114472076+micdra01@users.noreply.github.com>
2023-01-04 09:24:54 +0100

    Populated folders

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainWindow.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/Views/MainWindowView.fxml"));
         Parent root = loader.load();
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
