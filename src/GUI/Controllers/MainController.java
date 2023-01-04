28251a3f7a9026c70c758a4b1354b97c2c80a886
micdra01 <114472076+micdra01@users.noreply.github.com>
2023-01-04 19:57:25 +0100

    Skift scenes i samme stage

--- src/GUI/Controllers/MainController.java
+++ src/GUI/Controllers/MainController.java
@@ -4,20 +4,25 @@ import BLL.MovieManager;
 import DAL.Interfaces.IMovieDAO;
 import DAL.MovieDAO;
 import GUI.Models.MovieModel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
-    public GridPane grid;
-
-    public AnchorPane contentBoxId;
+    @FXML
+    private BorderPane borderPane;
 
     private MovieModel movieModel;
 
@@ -41,5 +46,24 @@ public class MainController implements Initializable {
         }
 
 
+    }
+
+    public void handleHome(ActionEvent actionEvent) throws IOException {
+        VBox home = FXMLLoader.load(getClass().getResource("/GUI/Views/HomeView.fxml"));
+        borderPane.setCenter(home);
+    }
+
+    public void handleTrending(ActionEvent actionEvent) throws IOException {
+        VBox trending = FXMLLoader.load(getClass().getResource("/GUI/Views/TrendingView.fxml"));
+        borderPane.setCenter(trending);
+    }
+
+    public void handlePopular(ActionEvent actionEvent) {
+    }
+
+    public void handleFavorites(ActionEvent actionEvent) {
+    }
+
+    public void handleAddMovie(ActionEvent actionEvent) {
     }
 }
