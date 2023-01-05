8a4b1488c56a13bed6fdbd1c0b9a0e10acf2b15c
micdra01 <114472076+micdra01@users.noreply.github.com>
2023-01-05 17:32:45 +0100

    MovieCards added

--- src/GUI/Controllers/TrendingController.java
+++ src/GUI/Controllers/TrendingController.java
@@ -1,6 +1,7 @@
 package GUI.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -9,6 +10,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,21 +21,36 @@ public class TrendingController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //Create a grid in the ScrollPane to hold all movies
         GridPane grid = new GridPane();
         trending.setContent(grid);
-        Image img = new Image("/Images/play.PNG");
-        ImageView imgV1 = new ImageView(img);
-        imgV1.setSmooth(true);
-        imgV1.setPreserveRatio(true);
-        imgV1.setFitWidth(200);
-        ImageView imgV2 = new ImageView(img);
 
-        grid.add(imgV1, 0, 0);
-        grid.add(new Label(new String("Uhh?")), 0, 1);
+        try {
+            //Loads in the MovieCard fxml
+            GridPane movieCard1 = FXMLLoader.load(getClass().getResource("/GUI/Views/MovieCard.fxml"));
+            GridPane movieCard2 = FXMLLoader.load(getClass().getResource("/GUI/Views/MovieCard.fxml"));
+            GridPane movieCard3 = FXMLLoader.load(getClass().getResource("/GUI/Views/MovieCard.fxml"));
+            GridPane movieCard4 = FXMLLoader.load(getClass().getResource("/GUI/Views/MovieCard.fxml"));
 
-        grid.add(new Separator(), 1,0);
 
-        grid.add(imgV2, 7, 0);
-        grid.add(new Label(new String("Ahhhh!")), 2, 1);
+            //Creates an image & imageview, and fits the size (only for the first movie card)
+            Image img1 = new Image("/Images/play.PNG");
+            ImageView imgV1 = new ImageView(img1);
+            imgV1.setPreserveRatio(true);
+            imgV1.fitWidthProperty().bind(movieCard1.widthProperty());
+
+            //Adds the image to the movie card
+            movieCard1.add(imgV1,0,0);
+
+            //Adds the MovieCard to the grid
+            grid.add(movieCard1,0,0);
+            grid.add(movieCard2,1,1);
+            grid.add(movieCard3,2,2);
+            grid.add(movieCard4,3,3);
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
