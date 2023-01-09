c150b32955cb7579a9118dc6ebe2c1d1e77753e2
micdra01 <114472076+micdra01@users.noreply.github.com>
2023-01-09 21:00:55 +0100

    Opens movie in system's mediaplayer

--- src/GUI/Controllers/MovieController.java
+++ src/GUI/Controllers/MovieController.java
@@ -7,15 +7,20 @@ import GUI.Models.MovieModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.media.MediaView;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import java.util.ResourceBundle;
 
 public class MovieController implements Initializable {
@@ -113,7 +118,15 @@ public class MovieController implements Initializable {
             throw new RuntimeException(e);
         }
     }
-    
+
+    public void handlePlayMovie(ActionEvent actionEvent) {
+        try {
+            String moviePath = new File(movie.getMovieFileLink()).getAbsolutePath();
+            Desktop.getDesktop().open(new File(moviePath));
+        } catch (IOException e) {
+            new Exception("Failed to play movie"+e);
+        }
+    }
 }
 
 
